f55b2b0580e1631cdcfeb35e07ee6945098d202b
54chen <czhttp@gmail.com>
2022-08-12 15:43:17 +1200

    fix issue

--- src/main/java/com/john/common/WinRegistry.java
+++ src/main/java/com/john/common/WinRegistry.java
@@ -1,29 +1,46 @@
 package com.john.common;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
 
 public class WinRegistry {
 
-  private static final String REGQUERY_UTIL = "reg query ";
+  private static final String REGQUERY_UTIL = "REG QUERY";
 
   private static final String REGSTR_TOKEN = "REG_BINARY";
 
   public static byte[] getReg(String path, String name) {
+    String result = "";
+
+    String line = "";
+    ArrayList<String> result2 = new ArrayList<String>();
+
     try {
-      String[] cmd = {REGQUERY_UTIL, path, "/v", name};
-      System.out.println(String.join(" ",cmd));
+      String[] cmd = { REGQUERY_UTIL, path, "/v", name };
+      System.out.println(String.join(" ", cmd));
       Process process = Runtime.getRuntime().exec(cmd);
       StreamReader reader = new StreamReader(process.getInputStream());
+
+      BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      while ((line = input.readLine()) != null) {
+        result2.add(new String(line));
+      }
+      BufferedReader input2 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+      while ((line = input2.readLine()) != null) {
+        result2.add(new String(line));
+      }
+      int exitvalue = process.waitFor();
 
       reader.start();
       process.waitFor();
       reader.join();
 
-      String result = reader.getResult();
+      result = reader.getResult();
       int p = result.indexOf(REGSTR_TOKEN);
-      System.out.println(result);
 
       if (p == -1)
         return null;
@@ -35,6 +52,7 @@ public class WinRegistry {
       return null;
     }
   }
+
   public static void main(String[] args) {
     System.out.print(hexStringToByteArray("390FFC122947ECA986DEF4D20D51E1A0A9545D3BD2922C64CA4B8DDA0CA1C7AE"));
   }
